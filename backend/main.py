b8e476d47c2f3b9fab90ed1e2fd2185a444fd527
Saurav Chakravorty <saurav.chakravorty@jazzx.ai>
2025-06-12 09:50:21 -0700

    local again

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
         "http://localhost:5173", 
         "http://127.0.0.1:5173",
         "https://wealthcast.railway.internal",
-        "https://wealthcast-production.up.railway.app/"
+        "https://wealthcast-production.up.railway.app/", 
     ], 
     allow_credentials=True,
     allow_methods=["*"],
